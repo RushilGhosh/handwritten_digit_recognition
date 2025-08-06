acdf31fa22e833859742f5b03ee77c2e7f6f7ac2
RushilGhosh <rushilghosh@gmail.com>
2025-08-06 12:49:13 -0700

    Streamlit implementation, now runs but model is very inaccurate

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
 #premade dataset that we are loading into the environment
 mnist = tf.keras.datasets.mnist
@@ -28,11 +30,45 @@ model.add(tf.keras.layers.Dense(10, activation="softmax"))
 #note: this is inefficient because we are retraining the model every time we run the file but idc
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(x_train, y_train, epochs=3)
+model.fit(x_train, y_train, epochs=5)
 
-#model.save('handwritten.model')
+# model.save('handwritten.model')
 
 # loss, accuracy = model.evaluate(x_test, y_test)
 # print(loss)
 # print(accuracy)
+
+st.set_page_config(page_title="Digit Identifier", page_icon="🔢", layout="centered")
+st.title("Digit Identifier")
+st.write("Draw a one-digit number in the white space below and AI will guess what number you drew!")
+st.write("Please wait if the page turns dim, the page is loading the model.")
+canvas = st_canvas(
+    fill_color="000000",
+    stroke_width=10,
+    stroke_color="black",
+    background_color="white",
+    background_image=None,
+    update_streamlit="true",
+    height=500,
+    width=500,
+    drawing_mode="freedraw",
+    point_display_radius=0,
+    key="canvas"
+)
+
+btn=st.button("Predict")
+
+if btn:
+    st.empty()
+    if canvas.image_data is not None:
+        cv2.imwrite("canvas.png", canvas.image_data)
+        img=cv2.imread("canvas.png", cv2.IMREAD_GRAYSCALE)
+        resized=cv2.resize(img, (28,28)) #model's first layer flattens 28x28 image
+        normalized=resized/255.0 #normalizes each pixel's rgb value to be between 0 and 1
+        reshaped=np.reshape(normalized, (1,28,28))
+        prediction=model.predict(reshaped)
+        st.write(f"The number you drew is {np.argmax(prediction)}")
+
+    else:
+        st.error("Nothing has been drawn yet!")
 
